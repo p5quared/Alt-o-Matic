43a113aa224f03b2fcb4f9a5f7bf76cd0e54435e
Peter V <98245483+p5quared@users.noreply.github.com>
2023-04-29 11:19:04 -0400

    linkage

--- scripts/insert_alts.js
+++ scripts/insert_alts.js
@@ -7,20 +7,36 @@ function img_find() {
         if(all_images[i].alt === ""){
             images_no_alt.push(all_images[i]);
         }
-        else
-            console.log(all_images[i].alt)
-        
+        else{
+            // console.log(all_images[i].alt)
+        }
+
     }
     console.table(images_no_alt);
     for (let i = 0; i < images_no_alt.length; i++) {
-        insert_alt(images_no_alt[i]);
+        set_alt(images_no_alt[i]);
     }
 }
 
-function insert_alt(img){
-    for (let i = 0; i < img.length; i++) {
-        img[i].alt = "HOLY SHIT IT WORKED " + (i+1);
-    }
+function set_alt(img){
+    console.log("Attemping to fetch...")
+    const img_url = img.src
+    const url_protocol = img_url.split(":")[0]
+    const url_remaining = img_url.split(":")[1]
+    const fetch_url = "https://altomatic.fly.dev/test-params?proto=" + url_protocol + "&dest=" + url_remaining
+    fetch(fetch_url)
+        .then(response => {
+            console.log(response);
+            if (!response.ok) {
+                return Promise.reject(new Error('Failed to load'));
+            }else{
+                return response.json();
+            }
+        }).then(data => {
+        console.log('DATA:', data);
+    }).catch(error => {
+        console.error('Error:', error);
+    })
 }
 img_find();
 
